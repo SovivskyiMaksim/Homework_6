f20534af425deddd7134ac0351be773dd1999adc
sovma <sov.maks73@gmail.com>
2023-10-30 22:00:59 +0200

    Домашка 6

---
+++ Homework_6.py
@@ -0,0 +1,68 @@
+#1)
+
+# my_int = 2003850506600
+# my_str = str(my_int)
+# print(my_str.count("0"))
+
+#2)
+
+# my_int = 2003850506600
+# my_str = str(my_int)
+# count = 0
+# for i in reversed(my_str):
+#     if i == "0":
+#         count +=1
+#     else:
+#         break
+# print(count)
+
+#3)
+
+# my_list_1 = [1, "Hello", [32], 2.0]
+# my_list_2 = ["Hi", 785, True, 6.8]
+#
+# my_result = []
+#
+# for i in range(len(my_list_1)):
+#     if i % 2 == 0:
+#         my_result.append(my_list_1[i])
+#
+# for i in range(len(my_list_2)):
+#     if i % 2 == 0:
+#         my_result.append(my_list_2[i])
+#
+# print(my_result)
+
+#4)
+
+# my_list = [1,2,3,4]
+# new_list = [8,9,3,5]
+# new_list.append(my_list[0])
+# print(new_list)
+
+#5)
+
+# my_list = [1,2,3,4]
+# x = my_list.pop(0)
+# my_list.append(x)
+# print(my_list)
+
+#6)
+
+# my_str = "43 більше ніж 34, але менше ніж 56"
+# x = my_str.split()
+#
+# total_sum = 0
+#
+# for i in x:
+#     i = i.strip(",.;:!")
+#     if i.isdigit():
+#         total_sum += int(i)
+#
+# print(total_sum)
+
+#7)
+
+my_list = [4,6,9,0,1,3,5]
+#thtrhntyrntynty
+my_list = [4,6,9,0,1,3,5]
